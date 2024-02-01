dd1d5112dc73de377e011f26b34da3a6df538f40
BastienBenech <bastbenech@gmail.com>
2024-02-01 11:04:34 +0100

    refactor: replace switch by ifs + move commands interpretation inside new class interpreter

--- src/serverRover.ts
+++ src/serverRover.ts
@@ -2,12 +2,11 @@ import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import { Rover } from "./classes/Rover";
-import { RoverInterpreteur } from "./interpreteurs/RoverInterpreteur";
 import { Direction } from "./enums/Direction";
 import { Position } from "./classes/Position";
 import { Coordinates } from "./classes/Coordinates";
 import { Planete } from "./classes/Planete";
-import {Inputs} from "./enums/Inputs";
+import {Interpreter} from "./interpreteurs/Interpreter";
 
 const app = express();
 const httpServer = createServer(app);
@@ -17,6 +16,7 @@ const positionRover = new Position(coordonneesRover, Direction.Est);
 const planete = new Planete(15, 15);
 planete.generateObstacle(5);
 const rover = new Rover(positionRover, planete);
+const interpreter = new Interpreter();
 
 io.on("connection", (socket: Socket) => {
     console.log("Rover connecté !");
@@ -27,76 +27,8 @@ io.on("connection", (socket: Socket) => {
 
         const listeCommandes = commandes.split('');
         let messageResponse = '\nCoordonnées initiales : { X: ' + rover.getCoordinates().x + ', Y: ' + rover.getCoordinates().y + ' } ' + 'Direction : ' + rover.getDirection();
-
-        for (let i = 0; i < listeCommandes.length; i++) {
-            switch (listeCommandes[i].toLowerCase()) {
-                case Inputs.Avancer:
-                    if (!rover.checkObstacleForward()) {
-                        rover.avancer();
-                        messageResponse = messageResponse +
-                            '\nCoordonnées : { X: ' + rover.getCoordinates().x + ', Y: ' + rover.getCoordinates().y + ' } ' +
-                            'Direction : ' +
-                            rover.getDirection()
-                        ;
-                    } else {
-                        messageResponse = messageResponse +
-                            '\nImpossible d\'avancer, obstacle en { X: ' +
-                            rover.getNextPosition().x +
-                            ', Y: ' +
-                            rover.getNextPosition().y +
-                            ' }'
-                        ;
-                    }
-                    break;
-                case Inputs.Reculer:
-                    if (!rover.checkObstacleBackward()) {
-                        rover.reculer();
-                        messageResponse = messageResponse +
-                            '\nCoordonnées : { X: ' + rover.getCoordinates().x + ', Y: ' + rover.getCoordinates().y + ' } ' +
-                            'Direction : ' +
-                            rover.getDirection()
-                        ;
-                    } else {
-                        messageResponse = messageResponse +
-                            '\nImpossible de reculer, obstacle en { X: ' +
-                            rover.getNextPosition().x +
-                            ', Y: ' +
-                            rover.getNextPosition().y +
-                            ' }'
-                        ;
-                    }
-                    break;
-                case Inputs.Gauche:
-                    rover.tournerGauche();
-                    messageResponse = messageResponse +
-                        '\nCoordonnées : { X: ' + rover.getCoordinates().x + ', Y: ' + rover.getCoordinates().y + ' } ' +
-                        'Direction : ' +
-                        rover.getDirection()
-                    ;
-                    break;
-                case Inputs.Droite:
-                    rover.tournerDroite();
-                    messageResponse = messageResponse +
-                        '\nCoordonnées : { X: ' + rover.getCoordinates().x + ', Y: ' + rover.getCoordinates().y + ' } ' +
-                        'Direction : ' +
-                        rover.getDirection()
-                    ;
-                    break;
-                default:
-                    messageResponse = messageResponse +
-                        "\nCommande invalide. [" +
-                        "\x1B[32m" +
-                        listeCommandes.slice(0, i).join(' ') +
-                        "\x1B[31m " +
-                        listeCommandes[i] +
-                        " \x1B[0m" +
-                        listeCommandes.slice(i+1, listeCommandes.length).join(' ') +
-                        "]"
-                    ;
-                    break;
-            }
-        }
-        socket.emit('response', messageResponse);
+        let finalMessageResponse = interpreter.interpretCommands(listeCommandes, rover, messageResponse);
+        socket.emit('response', finalMessageResponse );
     });
 });
 
