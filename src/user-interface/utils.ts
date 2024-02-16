f6f673bc72e8f2c9325a6d677399011b0e9bc670
TavaresDylan <tavares.dylan03@gmail.com>
2024-02-16 12:28:19 +0100

    refactor: avoid stdOut rl on first line

--- src/user-interface/utils.ts
+++ src/user-interface/utils.ts
@@ -1,7 +1,10 @@
 export const printOutput = (message: Array<string>) => {
     const stdoutElement = document.getElementById("textarea");
     for (let i = 0; i < message.length; i++) {
-        stdoutElement!.innerHTML += `\n> ${message[i]}`;
+        stdoutElement!.innerHTML +=
+            stdoutElement!.innerHTML.length > 0
+                ? `\n> ${message[i]}`
+                : `> ${message[i]}`;
     }
     stdoutElement!.scrollTop = stdoutElement!.scrollHeight;
 };
