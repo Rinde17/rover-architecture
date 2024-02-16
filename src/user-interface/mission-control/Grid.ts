cf602e3ba428202d70035cd62701e76b62733665
TavaresDylan <tavares.dylan03@gmail.com>
2024-02-16 14:13:38 +0100

    feat: grid written in VanJs (POC)

--- src/user-interface/mission-control/Grid.ts
+++ src/user-interface/mission-control/Grid.ts
@@ -1,9 +1,49 @@
 import van from "vanjs-core";
 
-const { table, tbody } = van.tags;
+const { table, tbody, tr, th, td } = van.tags;
 
 const Grid = () => {
-    return table({}, tbody());
+    const xIndex: Array<string> = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+    ];
+    const yIndex: Array<string> = [
+        "",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+    ];
+    return table(
+        {},
+        tbody(
+            {},
+            yIndex.map((yIndex, index) =>
+                tr({ id: index === 0 && "x-index" }, [
+                    th(yIndex),
+                    xIndex.map((_, index) => td(xIndex[index])),
+                ])
+            )
+        )
+    );
 };
 
 export default Grid;
